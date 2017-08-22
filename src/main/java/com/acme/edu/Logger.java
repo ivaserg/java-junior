67331497752f880b0bc0419cef4acdf984531c05
ivaserg <ivanov.svv@gmail.com>
2017-08-22 14:36:14 +0300

    Make logger singleton

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,33 @@
 package com.acme.edu;
 
 public class Logger {
+
+    private static Logger singletonLogger;
+
+    private Logger() {
+    }
+
+    public Logger createInstance() {
+        if (singletonLogger == null ) {
+            synchronized (Logger.class) {
+             if (singletonLogger == null) {
+                 singletonLogger = new Logger();
+             }
+            }
+        }
+        return singletonLogger;
+    }
+
     public static void log(int message) {
         System.out.println("primitive: " + message);
     }
 
     public static void log(byte message) {
         System.out.println("primitive: " + message);
+    }
+
+    public static void main(String[] args) {
+        Logger logger = new Logger();
+
     }
 }
