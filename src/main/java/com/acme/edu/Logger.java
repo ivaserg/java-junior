61efaf968fc6080ed4de646e6c1329a2f0950dbc
ivaserg <ivanov.svv@gmail.com>
2017-08-23 15:40:39 +0300

    Minor code cleanup

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -3,36 +3,33 @@ package com.acme.edu;
 
 public class Logger  {
 
-
     public static void log(int message) {
-        log("primitive: ", message);
+        log("primitive", message);
     }
 
     public static void log(byte message) {
-        log("primitive: ", message);
+        log("primitive", message);
     }
 
     public static void log(char message) {
-        log("char: ", message);
+        log("char", message);
     }
 
     public static void log(String message) {
-        log("string: ", message);
+        log("string", message);
     }
 
     public static void log(boolean message) {
-        log("primitive: ", message);
+        log("primitive", message);
     }
 
     public static void log(Object message) {
-        log("reference: ", message);
+        log("reference", message);
     }
 
-    private static void log(String appendString, Object message) {
-        System.out.println(appendString + message);
+    private static void log(String describeString, Object message) {
+        System.out.println(String.format("%s: %s",describeString, message));
     }
 
-    public static void main(String[] args) {
 
-    }
 }
